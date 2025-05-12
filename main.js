a6f8f5a7fa9b236d575a6b2c122bab46ff03e8ae
AAbAlQahtani <asmaalqhtani123@gmail.com>
2025-05-12 14:01:56 +0300

    check

--- main.js
+++ main.js
@@ -7,22 +7,55 @@ let img = document.getElementById("img")
 let userPost = document.getElementById("user-post")
 
 btn.addEventListener("click", () => {
-    fetch('https://68219a21259dad2655afc28a.mockapi.io/post', {
-        method: 'POST',
-        body: JSON.stringify({
-            username: username.value,
-            textarea: textarea.value,
-            img: img.src
 
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
+
+
+
+    let username1 = username.value.trim();
+    let textarea1 = textarea.value.trim();
+    let img1 = img.src
+
+    if (username1.length < 4) {
+        alert("username has to be more than > 3 characters");
+        return;
+    }
+
+    if (textarea1.length < 6) {
+        alert("text has to be longer than > 5 characters");
+        return;
+    }
+
+    if (!img1) {
+        alert("image is required");
+        return;
+    }
+    fetch(`https://68219a21259dad2655afc28a.mockapi.io/post?username=${encodeURIComponent(username1)}`)
+        .then((res) => res.json())
+        .then((matches) => {
+            if (matches.length > 0) {
+                alert("username exist already");
+                return;
+            }
+        })
+    fetch('https://68219a21259dad2655afc28a.mockapi.io/post',
+        {
+
+            method: 'POST',
+            body: JSON.stringify({
+                username: username.value,
+                textarea: textarea.value,
+                img: img.src
+
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
         .then((response) => response.json())
         .then((json) => console.log(json));
+}
 
-})
+)
 
 
 
